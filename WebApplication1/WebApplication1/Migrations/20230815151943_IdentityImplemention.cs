7833ffa554b1b2e4a79e8ae16262c8c2ac9c9586
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-08-16 13:21:11 -0500

    up

---
+++ WebApplication1/WebApplication1/Migrations/20230815151943_IdentityImplemention.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityImplemention : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
